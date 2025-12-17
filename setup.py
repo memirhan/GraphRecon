0e3cd606eac9b90eb1d1a9d5f55b9e2ff0935710
Muhammet Emirhan <158741458+memirhan@users.noreply.github.com>
2025-12-17 23:13:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="graphrecon",
-    version="1.0.0",
+    version="1.1.0",
     py_modules=["graphrecon"],
     install_requires=[
         "aiohttp>=3.9.0"
